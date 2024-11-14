395157240ace210137c018568c543f273298d76f
JIELSAN <jesussjg811@gmail.com>
2024-11-14 02:35:36 -0600

    PWA

---
+++ serviceworker.js
@@ -0,0 +1,61 @@
+//Crear la varieble de cache
+const currentCache = "cacheWiki-v1.0";
+
+const files = [
+    "/",
+    "index.html",
+    "img/logo.png",
+    "css/estilo_01.css",
+    "css/estilo_02.css"
+];
+
+//Instalar el Service Worker y cachear los recursos
+self.addEventListener("install", event => {
+    async function preCacheResources() {
+        //Abrir el cahe de la app
+        const cache = await caches.open(currentCache);
+        //Agregar los recursos a cache
+        cache.addAll(files);
+    }
+    event.waitUntil(preCacheResources());
+});
+
+//Interceptar solicitudes y responder con caché o red: 
+//monitorea cuando el usuario pide un recurso.
+self.addEventListener("fetch", event => {
+    async function navigateOrDisplayOfflinePage() {
+        try {
+            //Intentar cargar la pagina desde la web
+            const networkResponse = await fetch(event.request);
+            return networkResponse;
+        } catch (error) {
+            //Cuando el internet falle, el disposiivo esta offline
+            const cache = await caches.open(currentCache);
+            const cachedResponse = await cache.match("/offline");
+            return cachedResponse;
+        }
+    }
+
+    //Solo llama a event.respondWith() si se trata de una solicitud de navegación
+    //para una página HTML.
+    if(event.request.mode === 'navigate') {
+        event.respondWith(navigateOrDisplayOfflinePage());
+    }
+});
+
+//Activar el Service Worker y eliminar cachés viejos
+self.addEventListener("activate", event => {
+    async function deleteOldCaches() {
+        //Enumere todas las cachés por sus nombres.
+        const names = await caches.keys();
+        await Promise.all(names.map(name => {
+            if (name !== currentCache) {
+                //Si el nombre de una caché es el nombre actual, elimínela.
+                return caches.delete(name);
+            }
+        }));
+    }
+
+    event.waitUntil(deleteOldCaches());
+});
+
